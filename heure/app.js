f7d54fc87e9a02657d50aaf44176df13bdd6d629
Arnaud BODEL <contact@osw3.net>
2022-09-09 15:15:37 +0200

    -

--- heure/app.js
+++ heure/app.js
@@ -6,3 +6,6 @@ var minutes = date.getMinutes();
 var secondes = date.getSeconds();
 
 // Affiche l'heure au format HH:ii:ss dans la console toute les secondes
+// -> 15:16:32
+// -> 15:16:33
+// -> 15:16:34
