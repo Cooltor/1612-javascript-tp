39634ff314e0e5e9de3d829f4cb8df03a8c0738b
Arnaud BODEL <contact@osw3.net>
2022-09-13 14:38:42 +0200

    -

---
+++ DOM/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const title = "Super Car 3000 - les bagnolles du turfu !";
+const cars = [
+    {
+        brand: "Ford",
+        model: "Focus",
+        motor: "gas",
+        isNew: false,
+    },
+    {
+        brand: "Ford",
+        model: "F-350",
+        motor: "gas",
+        isNew: false,
+    },
+    {
+        brand: "Mustang",
+        model: "Mach-e",
+        motor: "electrical",
+    },
+    {
+        brand: "Tesla",
+        model: "Model X",
+        motor: "electrical",
+    },
+    {
+        brand: "Toyota",
+        model: "Hilux",
+        motor: "gas",
+    },
+    {
+        brand: "DMC",
+        model: "Delorean '22",
+        motor: "electrical",
+        isNew: true,
+    },
+];
+
+const body = document.body;
+
+// -----------------------------------------------------------------------------
