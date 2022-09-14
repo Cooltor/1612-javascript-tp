6b48b69a24e38cb1f8f68bc8fda8f0b867375b3d
Arnaud BODEL <contact@osw3.net>
2022-09-14 15:42:16 +0200

    -

---
+++ form/assets/scripts/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// Generate Birthday <option>
+// -------------------------
+
+// Generate birthday[day] <option>
+// --
+
+// Generate birthday[month] <option>
+// --
+
+// Generate birthday[year] <option>
+// --
+
+
+
+// Add "required" symbol to <label>
+// -------------------------
+
+
+
+
+// Form checking
+// -------------------------
+
+// Catch the form submit
+// --
+
+// Retrieve form data
+// --
+
+// Validate form data + Add error message
+// --
